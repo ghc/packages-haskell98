7cb11d9242ac1515b365db00d28e72af9fd258fd
simonmar <unknown>
2002-04-02 09:46:26 +0000

    [project @ 2002-04-02 09:46:26 by simonmar] Hide the non-H98 bits exported by System.Locale

--- Locale.hs
+++ Locale.hs
@@ -2,4 +2,9 @@ module Locale (
     module System.Locale
   ) where
 
-import System.Locale
+import System.Locale ( 
+	-- just the bits that are specified by Haskell 98
+	TimeLocale(wDays,months,amPm,dateTimeFmt,
+		   dateFmt,timeFmt,time12Fmt),
+        defaultTimeLocale
+    )
