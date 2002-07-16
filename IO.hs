8416d7e9b1d145e56b6bf7a418f6b71096e33ea4
ross <unknown>
2002-07-16 16:09:00 +0000

    [project @ 2002-07-16 16:09:00 by ross] Add imports of Hugs.* modules (wrapped in #ifdef __HUGS__) to make these modules work with Hugs.

--- IO.hs
+++ IO.hs
@@ -1,5 +1,10 @@
+{-# OPTIONS -cpp #-}
 module IO (
     module System.IO
   ) where
 
+#ifdef __HUGS__
+import System.IO hiding ( fixIO )
+#else
 import System.IO hiding ( fixIO, hGetBuf, hPutBuf )
+#endif
