c5a9db3be96189bb41653a6bf6fb177d4ae1a635
malcolm <unknown>
2002-09-16 13:54:19 +0000

    [project @ 2002-09-16 13:54:19 by malcolm] Export the constructor `TimeLocale' as per the Haskell'98 Report.

--- Locale.hs
+++ Locale.hs
@@ -4,7 +4,7 @@ module Locale (
 
 import System.Locale ( 
 	-- just the bits that are specified by Haskell 98
-	TimeLocale(wDays,months,amPm,dateTimeFmt,
+	TimeLocale(TimeLocale,wDays,months,amPm,dateTimeFmt,
 		   dateFmt,timeFmt,time12Fmt),
         defaultTimeLocale
     )
