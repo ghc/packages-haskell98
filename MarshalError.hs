ae043e4901944a210a1386a5045c63d696b17824
simonmar <unknown>
2002-12-04 12:23:44 +0000

    [project @ 2002-12-04 12:23:44 by simonmar] MarshalError is supposed to export some of the stuff from System.IO.Error too.

--- MarshalError.hs
+++ MarshalError.hs
@@ -1,2 +1,17 @@
-module MarshalError (module Foreign.Marshal.Error) where
+module MarshalError (
+  	module Foreign.Marshal.Error,
+	IOErrorType,
+	mkIOError,
+	alreadyExistsErrorType,
+	doesNotExistErrorType,
+	alreadyInUseErrorType,
+	fullErrorType,
+	eofErrorType,
+	illegalOperationType,
+	permissionErrorType,
+	userErrorType,
+	annotateIOError
+  ) where
+
+import System.IO.Error
 import Foreign.Marshal.Error
