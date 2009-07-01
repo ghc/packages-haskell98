a4df0030bfc7d0a441150bf9cf8e98a816c17aaa
Max Bolingbroke <batterseapower@hotmail.com>
2009-07-01 20:04:32 +0000

    Support for -fwarn-unused-do-bind and -fwarn-wrong-do-bind, as per #3263

--- IO.hs
+++ IO.hs
@@ -37,7 +37,7 @@ bracket        :: IO a -> (a -> IO b) -> (a -> IO c) -> IO c
 bracket before after m = do
         x  <- before
         rs <- try (m x)
-        after x
+        _ <- after x
         case rs of
            Right r -> return r
            Left  e -> ioError e
@@ -52,7 +52,7 @@ bracket_        :: IO a -> (a -> IO b) -> IO c -> IO c
 bracket_ before after m = do
          x  <- before
          rs <- try m
-         after x
+         _ <- after x
          case rs of
             Right r -> return r
             Left  e -> ioError e
