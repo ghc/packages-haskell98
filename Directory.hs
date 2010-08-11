d81534d31b7926789e7ed4505024430ab87cbcb9
Ian Lynagh <igloo@earth.li>
2010-08-11 19:04:19 +0000

    Add our own Permissions type, now System.Directory has deviated frmo H98

--- Directory.hs
+++ Directory.hs
@@ -8,4 +8,32 @@ module Directory (
     getModificationTime 
   ) where
 
-import System.Directory
+import System.Directory hiding (Permissions,
+                                readable, writable, executable, searchable,
+                                getPermissions, setPermissions)
+import qualified System.Directory as SD
+
+data Permissions = Permissions {
+                       readable   :: Bool,
+                       writable   :: Bool,
+                       executable :: Bool,
+                       searchable :: Bool
+                   }
+    deriving (Eq, Ord, Read, Show)
+
+getPermissions :: FilePath -> IO Permissions
+getPermissions fp = do perm <- SD.getPermissions fp
+                       return $ Permissions {
+                                    readable   = SD.readable perm,
+                                    writable   = SD.writable perm,
+                                    executable = SD.executable perm,
+                                    searchable = SD.searchable perm
+                                }
+
+setPermissions :: FilePath -> Permissions -> IO ()
+setPermissions fp perm = let mkPerm = setOwnerReadable   (readable   perm)
+                                    . setOwnerWritable   (writable  perm)
+                                    . setOwnerExecutable (executable perm)
+                                    . setOwnerSearchable (searchable perm)
+                         in SD.setPermissions fp (mkPerm emptyPermissions)
+
